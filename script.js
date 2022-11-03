62ce148e739253909a16e13a98d57b4ccb39aac4
Niessie <108128961+Niessie@users.noreply.github.com>
2022-11-03 12:20:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.alert("Dit is een ingebouwde functie ");
+
+
+function drukknop() {
+window.alert("Hoi u heeft zojuist op de knop gedrukt.");
+}
+
+var getal1=891;
+var getal2=198;
+var som=getal1 + getal2;
+document.write(som);
+
